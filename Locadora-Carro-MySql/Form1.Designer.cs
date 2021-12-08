aac2c0f4894214a03e7d7bacf4013c1b80bedd72
Vitor Lira <vitorlirasti@hotmail.com>
2021-12-08 15:53:05 -0300

    Inserindo Clientes

--- Locadora-Carro-MySql/Form1.Designer.cs
+++ Locadora-Carro-MySql/Form1.Designer.cs
@@ -30,12 +30,128 @@ namespace Locadora_Carro_MySql
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.Nome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCPF = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.txtidVeiculo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(12, 9);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(40, 15);
+            this.Nome.TabIndex = 0;
+            this.Nome.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CPF";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefone";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 27);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 23);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtCPF
+            // 
+            this.txtCPF.Location = new System.Drawing.Point(12, 82);
+            this.txtCPF.Name = "txtCPF";
+            this.txtCPF.Size = new System.Drawing.Size(100, 23);
+            this.txtCPF.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(12, 137);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 23);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtidVeiculo
+            // 
+            this.txtidVeiculo.Location = new System.Drawing.Point(12, 191);
+            this.txtidVeiculo.Name = "txtidVeiculo";
+            this.txtidVeiculo.Size = new System.Drawing.Size(100, 23);
+            this.txtidVeiculo.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Id do veiculo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(519, 319);
+            this.Controls.Add(this.txtidVeiculo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtCPF);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nome);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCPF;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox txtidVeiculo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Locadora-Carro-MySql/Form1.cs
+++ Locadora-Carro-MySql/Form1.cs
@@ -7,15 +7,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Locadora_Carro_MySql
 {
     public partial class Form1 : Form
     {
+        MySqlConnection conexao;
+        string data_source = "Server=localhost;user id=root;password=root;DATABASE=Locadora";
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //conectando com Preparing Statements
+                conexao = new MySqlConnection(data_source);
+
+                //abertura da conexao
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+
+                //definindo qual vai ser a conexao
+                cmd.Connection = conexao;
+
+                cmd.Prepare(); //preparo do comando de execução
+
+                cmd.CommandText = "INSERT INTO cliente(nome,cpf,telefone,fk_idveiculo) " + //comando do SQL que vai ser executados
+                                          " VALUES " +
+                                          "(@nome, @cpf, @telefone, @fk_idveiculo) ";
+
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text); // criando paramentro e adicionando valores neles
+                cmd.Parameters.AddWithValue("@cpf", txtCPF.Text);
+                cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text);
+                cmd.Parameters.AddWithValue("@fk_idveiculo", txtidVeiculo.Text);
+
+                cmd.ExecuteNonQuery(); //execução do comando
+
+                MessageBox.Show("Contato Inserido");
+                txtNome.Clear();
+                txtCPF.Clear();
+                txtTelefone.Clear();
+                txtidVeiculo.Clear();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message,
+                                "Erro",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu: " + ex.Message,
+                                "Erro",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+        }
     }
 }
